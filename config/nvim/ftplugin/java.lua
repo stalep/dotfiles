8b8929e1c3ed4fffd9c73a64d28f018e14f15b96
Ståle Pedersen <stalep@gmail.com>
2022-11-05 22:47:38 +0100

    added nvim-jdtls

--- config/nvim/ftplugin/java.lua
+++ config/nvim/ftplugin/java.lua
@@ -1,73 +1,44 @@
--- If you started neovim within `~/dev/xy/project-1` this would resolve to `project-1`
-local fn = vim.fn
-local project_name = fn.fnamemodify(fn.getcwd(), ':p:h:t')
+-- local config = {
+    -- cmd = {'/home/stalep/jdtls/bin/jdtls'},
+    -- root_dir = vim.fs.dirname(vim.fs.find({'.gradlew', '.git', 'mvnw'}, { upward = true })[1]),
+-- }
 
-local home_dir = os.getenv('HOME')
-local jabba_jdk_dir = home_dir..'/.jabba/jdk/'
-local jdk8_dir = jabba_jdk_dir..'1.8.0/Contents/Home'
-local nvim_dir = home_dir..'/.config/nvim'
-local rule_dir = nvim_dir..'/rule/'
-local java_settings_url = rule_dir..'settings.prefs'
-local java_format_style_rule = rule_dir..'eclipse-java-google-style.xml'
-local java_debug_jar = nvim_dir..'/java-debug/*.jar'
-local workspace_root_dir = nvim_dir..'/workspace/'
-local workspace_dir = workspace_root_dir..project_name
-local lombok_jar = vim.fn.expand(home_dir..'/.m2/repository/org/projectlombok/lombok/*/*.jar')
-local lsp = require('lspconfig')
-
-local on_attach = function(client, bufnr)
-  lsp.on_attach(client, bufnr)
-  lsp.navic_attach_and_setup(client, bufnr)
-
-  -- With `hotcodereplace = 'auto' the debug adapter will try to apply code changes
-  -- you make during a debug session immediately.
-  -- Remove the option if you do not want that.
-  require("jdtls").setup_dap({ hotcodereplace = "auto" })
-  require("jdtls.setup").add_commands()
-  require("jdtls.dap").setup_dap_main_class_configs()
-end
-
-local is_file_exist = function(path)
-  local f = io.open(path, 'r')
-  return f ~= nil and io.close(f)
-end
-
-local get_lombok_javaagent = function()
-  if is_file_exist(lombok_jar) then
-    return string.format('--jvm-arg=-javaagent:%s', lombok_jar)
-  end
-  return ''
-end
-
-local get_lombok_bootclasspath = function()
-  if is_file_exist(lombok_jar) then
-    return string.format('--jvm-arg=-Xbootclasspath/a:%s', lombok_jar)
-  end
-  return ''
-end
-
-local get_java_debug_jar = function()
-  local jdj_full_path = fn.expand(java_debug_jar)
-  if is_file_exist(jdj_full_path) then
-    return jdj_full_path
-  end
-  return ''
-end
-
--- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
--- Watch out for the 💀, it indicates that you must adjust something.
 local config = {
+  -- The command that starts the language server
+  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
   cmd = {
 
     -- 💀
-    'jdtls',
+    'java', -- or '/path/to/java17_or_newer/bin/java'
+            -- depends on if `java` is in your $PATH env variable and if it points to the right version.
 
-    get_lombok_javaagent(),
-    get_lombok_bootclasspath(),
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xms1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+
+    -- 💀
+    '-jar', '/home/stalep/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar',
+         -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+         -- Must point to the                                                     Change this to
+         -- eclipse.jdt.ls installation                                           the actual version
+
+
+    -- 💀
+    '-configuration', '/home/stalep/jdtls/config_linux',
+                    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+                    -- Must point to the                      Change to one of `linux`, `win` or `mac`
+                    -- eclipse.jdt.ls installation            Depending on your system.
+
 
     -- 💀
     -- See `data directory configuration` section in the README
-    '-data', workspace_dir,
+    '-data', '/home/stalep/.cache/workspace/folder'
   },
 
   -- 💀
@@ -79,49 +50,7 @@ local config = {
   -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
   -- for a list of options
   settings = {
-    ['java.settings.url'] = java_settings_url,
     java = {
-      configuration = {
-        -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-        -- And search for `interface RuntimeOption`
-        -- The `name` is NOT arbitrary, but must match one of the elements from `enum ExecutionEnvironment` in the link above
-        runtimes = {
-          {
-            name = 'JavaSE-1.8',
-            path = jdk8_dir
-          }
-        }
-      },
-      codeGeneration = {
-        hashCodeEquals = {
-          useInstanceof = true,
-          useJava7Objects = true
-        },
-        toString = {
-          codeStyle = "STRING_BUILDER_CHAINED"
-        },
-        useBlocks = true,
-      },
-      contentProvider = { preferred = 'fernflower' },
-      implementationsCodeLens = {
-        enabled = true
-      },
-      referencesCodeLens = {
-        enabled = true
-      },
-      signatureHelp = { enabled = true },
-      sources = {
-        organizeImports = {
-          starThreshold = 9999,
-          staticStarThreshold = 9999,
-        }
-      },
-      format = {
-        settings = {
-          url = java_format_style_rule,
-          profile = 'GoogleStyle'
-        }
-      }
     }
   },
 
@@ -133,18 +62,8 @@ local config = {
   --
   -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
   init_options = {
-    bundles = {
-      get_java_debug_jar()
-    }
+    bundles = {}
   },
-
---  capabilities = lsp.get_capabilities(),
-  flags = {
-    allow_incremental_sync = true,
-    debounce_text_changes = 150,
-    server_side_fuzzy_completion = true
-  },
-  ['on_attach'] = on_attach,
 }
 -- This starts a new client & server,
 -- or attaches to an existing client & server depending on the `root_dir`.
--- config/nvim/lua/stalep/plugins.lua
+++ config/nvim/lua/stalep/plugins.lua
@@ -78,12 +78,13 @@ return packer.startup(function(use)
   use { "rafamadriz/friendly-snippets", commit = "2be79d8a9b03d4175ba6b3d14b082680de1b31b1" } -- a bunch of snippets to use
 
   -- LSP
-  -- use { "williamboman/nvim-lsp-installer", commit = "e9f13d7acaa60aff91c58b923002228668c8c9e6" } -- simple to use language server installer
+  use { "williamboman/nvim-lsp-installer", commit = "e9f13d7acaa60aff91c58b923002228668c8c9e6" } -- simple to use language server installer
   use { "neovim/nvim-lspconfig", commit = "f11fdff7e8b5b415e5ef1837bdcdd37ea6764dda" } -- enable LSP
   use { "williamboman/mason.nvim", commit = "c2002d7a6b5a72ba02388548cfaf420b864fbc12"}
   use { "williamboman/mason-lspconfig.nvim", commit = "0051870dd728f4988110a1b2d47f4a4510213e31" }
   use { "jose-elias-alvarez/null-ls.nvim", commit = "c0c19f32b614b3921e17886c541c13a72748d450" } -- for formatters and linters
   use { "RRethy/vim-illuminate", commit = "a2e8476af3f3e993bb0d6477438aad3096512e42" }
+  use 'mfussenegger/nvim-jdtls' -- nvim-jdtls
 
   -- Telescope
   use { "nvim-telescope/telescope.nvim", commit = "76ea9a898d3307244dce3573392dcf2cc38f340f" }
